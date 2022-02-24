c6008ecf11ce626af70c95038b97380d16593290
amir <amirgh1380@gmail.com>
2022-02-24 15:26:47 +0330

    some refactoring in Tile.js

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,17 +2,11 @@ const Tile = ({ tile }) => {
   let classArray = ["tile"];
   classArray.push("tile" + tile.value);
 
-  if (!tile.mergedInto) {
-    classArray.push(`position_${tile.row}_${tile.column}`);
-  }
+  if (!tile.mergedInto) classArray.push(`position_${tile.row}_${tile.column}`);
 
-  if (tile.mergedInto) {
-    classArray.push("merged");
-  }
+  if (tile.mergedInto) classArray.push("merged");
 
-  if (tile.isNew()) {
-    classArray.push("new");
-  }
+  if (tile.isNew()) classArray.push("new");
 
   if (tile.hasMoved()) {
     classArray.push(`row_from_${tile.fromRow()}_to_${tile.toRow()}`);
@@ -20,7 +14,7 @@ const Tile = ({ tile }) => {
     classArray.push("isMoving");
   }
 
-  let calsses = classArray.join(' ');
+  const calsses = classArray.join(" ");
 
   return <span className={calsses}></span>;
 };
