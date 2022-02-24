57461277b105400a7822b3ba36f27a777de557ad
amir <amirgh1380@gmail.com>
2022-02-24 17:46:11 +0330

    refactor for styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./assets/styles/main.scss";
-import "./assets/styles/styles.scss";
+import "./assets/styles/app.scss";
 
 ReactDOM.render(
   <React.StrictMode>
